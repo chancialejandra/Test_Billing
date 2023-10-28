0371e6f40cb95ba27e57d61cf5a8dd603f0052d6
chancialejandra <99815344+chancialejandra@users.noreply.github.com>
2023-10-28 14:17:08 -0500

    add entities

--- src/main/java/com/test/technique/Billing/models/Bill.java
+++ src/main/java/com/test/technique/Billing/models/Bill.java
@@ -1,4 +1,30 @@
 package com.test.technique.Billing.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "`bill`")
 public class Bill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Column(name = "id_bill")
+    private Long idBill;
+
+    @Column(name = "total_amount")
+    private double totalAmount;
+
+    private String des;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST})
+    @JoinColumn(name = "id_user")
+    private User user;
 }
--- src/main/java/com/test/technique/Billing/models/User.java
+++ src/main/java/com/test/technique/Billing/models/User.java
@@ -1,4 +1,30 @@
 package com.test.technique.Billing.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "`user`")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name= "id_user")
+    private Long idUser;
+    private String name;
+
+    private Integer age;
+
+    private String email;
+
+    @OneToMany(mappedBy = "user",fetch = FetchType.EAGER,cascade = {CascadeType.ALL})
+    private List<Bill> bills;
 }
