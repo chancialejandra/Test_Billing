61b3e073068932d6928c0c2c05e82effddf15747
chancialejandra <99815344+chancialejandra@users.noreply.github.com>
2023-10-29 22:11:22 -0500

    Add get a user and their invoices

--- src/main/java/com/test/technique/Billing/dto/Request/BillRequest.java
+++ src/main/java/com/test/technique/Billing/dto/Request/BillRequest.java
@@ -2,7 +2,6 @@ package com.test.technique.Billing.dto.Request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.test.technique.Billing.models.User;
 import lombok.Builder;
 import lombok.Data;
 
